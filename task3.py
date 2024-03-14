ccefa8af07337c4469be508f3fa228f5369b8d4b
Abdekader Esaam <118766308+Eouronymous9990@users.noreply.github.com>
2024-03-14 15:21:58 +0200

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+
+video_path = 'test.mp4'
+cap = cv2.VideoCapture(video_path)
+
+if not cap.isOpened():
+    print("Error")
+    exit()
+
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    frame_path = f'frame_{frame_count}.jpg'
+    cv2.imwrite(frame_path, frame)
+
+
+cap.release()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
