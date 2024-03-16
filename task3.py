152e428aa61b7b5585b636edc2373754db9fd1fb
Abdekader Esaam <118766308+Eouronymous9990@users.noreply.github.com>
2024-03-16 22:07:46 +0200

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,41 +1,28 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import cv2
+import os
 
-video_path = 'test.mp4'
-cap = cv2.VideoCapture(video_path)
+def save_frames(v_path, f_folder):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-if not cap.isOpened():
-    print("Error")
-    exit()
+    cap = cv2.VideoCapture(video_path)
+    frame_count = 0
 
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_count += 1
+        path = f"frame_{frame_count}.jpg"
+        final_path = os.path.join(output_folder, path)
+        cv2.imwrite(final_path, frame)
 
-while True:
-    ret, frame = cap.read()
-    
-
-    if not ret:
-        break
-    cv2.resize(frame ,(280,280))
-    frame_path = f'frame_{frame_count}.jpg'
-    cv2.imwrite(frame_path, frame)
-
-
-cap.release()
+    cap.release()
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
+farmes = r"C:\Users\zbook 17 g3\Downloads\test___.mp4"
+folder = r"C:\Users\zbook 17 g3\Downloads\New folder (2)"
+save_frames(farmes, folder)
 
 
 
